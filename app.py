53a82f7eae20c673facd7027026a910c4dda38e8
Dylan Barker <dylanbarker59@gmail.com>
2020-09-10 21:02:00 +0100

    Validation for empty fields

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session, url_for
 
 app = Flask(__name__)
+wsgi_app = app.wsgi_app
+app.secret_key = 'somesecretkey'
 
 
 @app.route('/')
@@ -23,15 +25,32 @@ def contact():
     return render_template('contact.html', team=team)
 
 
-@app.route('/signup')
+@app.route('/signup', methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        entries = []
+        name = request.form.get("name")
+        age = request.form.get("age")
+        number = request.form.get("number")
+        gender = request.form.get("gender")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        postcode = request.form.get("postcode")
+        role = request.form.get("role")
+        s_d = request.form.get("speciality_or_disabled")
+        image = request.files["image"]
+        entries.extend((name, age, number, gender, email, password, postcode, role, s_d, image))
+        for i in entries:
+            if not i:
+                error = "Please fill in all fields"
+                return render_template('signup.html', error=error, name=name, user_age=age, number=number, s_d=s_d,
+                                       gender=gender, email=email, password=password, postcode=postcode, role=role,
+                                       image=image)
+        success = "Sign up has succeeded, please login"
+        return render_template('login.html', success=success)
     return render_template('signup.html')
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
-
-
-if __name__ == '__main__':
-    app.run()
