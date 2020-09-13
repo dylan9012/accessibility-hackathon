40a84b211f588713b4341a9740ecfa4af99e007f
dylan9012 <dylanbarker59@gmail.com>
2020-09-13 17:27:45 +0100

    Finishing up other login pages

--- app.py
+++ app.py
@@ -8,9 +8,17 @@ app.secret_key = 'e471e07eb0c2977afd4f398907cb78f8'
 
 exampleUser = ["1", "Dylan Barker", "18", "Vision impairment", "E3 3NR", "Disabled", "dylanbarker59@gmail.com", "male",
                "07756756382", "destiny1"]
-potential_matches = [["Eyuael Berhe", "Male", "18", "Vision impairment", "5"],
-                     ["Isaac Addo", "Male", "18", "Deaf or hard of hearing", "6"],
-                     ["Muhriz Tauseef", "Male", "18", "Autism Spectrum Disorder", "7"]]
+potential_matches = [["1", "Eyuael Berhe", "Male", "18", "5"],
+                     ["2", "Isaac Addo", "Male", "18", "6"],
+                     ["3", "Muhriz Tauseef", "Female", "18", "7"]]
+
+likes = [["1", "Eyuael Berhe", "Male", "18", "5"],
+         ["2", "Isaac Addo", "Male", "18", "6"],
+         ["3", "Muhriz Tauseef", "Female", "18", "7"]]
+
+matched = [["1", "Eyuael Berhe", "Male", "eyuaelberhe@gmail.com", "07765765382"],
+           ["2", "Isaac Addo", "Male", "isaacaddo1714@gmail.com", "07962390386"],
+           ["3", "Muhriz Tauseef", "Female", "muhriztauseef82@gmail.com", "0794349631"]]
 
 
 @app.route('/')
@@ -80,25 +88,6 @@ def login():
     return render_template('one/login.html')
 
 
-@app.route('/dashboard', methods=["GET", "POST"])
-def dashboard():
-    if "id" in session:
-        if "d-page" in session:
-            session["d-page"] += 1
-            name = potential_matches[session["d-page"]][0]
-            gender = potential_matches[session["d-page"]][1]
-            age = potential_matches[session["d-page"]][2]
-            s_d = potential_matches[session["d-page"]][3]
-            distance = potential_matches[session["d-page"]][4]
-            
-
-        else:
-            ...
-    else:
-        return redirect(url_for('login'))
-    return render_template('two/dashboard.html', name=exampleUser[1])
-
-
 @app.route('/logout')
 def logout():
     if "id" in session:
@@ -106,18 +95,83 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/matches', methods=["GET", "POST"])
+def get_information():
+    name = potential_matches[session["page"]][1]
+    gender = potential_matches[session["page"]][2]
+    age = potential_matches[session["page"]][3]
+    distance = potential_matches[session["page"]][4]
+    return name, gender, age, distance
+
+
+@app.route('/dashboard', methods=["GET", "POST"])
+def dashboard():
+    if "id" not in session:
+        return redirect(url_for('login'))
+
+    if request.method == "POST":
+        decision = request.form.get("decision")
+        if decision == "Match":
+            ...  # - Add to match table
+        else:
+            ...  # - Add entry to like table saying not interested
+
+    if "page" in session:
+        session["page"] += 1
+    else:
+        session["page"] = 0
+    # - Run query to get potential matches here
+    if len(potential_matches) > session["page"]:
+        name, gender, age, distance = get_information()
+        return render_template('two/dashboard.html', name=name, gender=gender, age=age, distance=distance)
+    else:
+        potential_matches.clear()  # - How often do we query the database for matches: Each login
+        session.pop("page", None)
+        return render_template('two/dashboard.html', end=True)
+
+
+@app.route('/requests', methods=["GET", "POST"])
+def requests():
+    if "id" not in session:
+        return redirect(url_for('login'))
+    if request.method == "POST":
+        selected = None
+        for i in range(len(likes)):
+            decision_user_id = likes[i][0]
+            decision = request.form.get("decision" + decision_user_id)
+            if not decision or decision == "Select":
+                continue
+            else:
+                selected = i
+        likes.pop(selected) if selected is not None else flash("Choose a decision before pressing submit", "danger")
+
+        # - Do query to insert here
+    # - Query to get their likes
+    end = False
+    if not likes:
+        end = True
+    return render_template('two/requests.html', users=likes, end=end)
+
+
+@app.route('/matches', methods=["GET"])
 def matches():
     if "id" not in session:
         return redirect(url_for('login'))
-    return render_template('two/matches.html')
+    return render_template('two/matches.html', matches=matched)
 
 
 @app.route('/profile', methods=["GET"])
 def profile():
     if "id" not in session:
         return redirect(url_for('login'))
-    return render_template('two/profile.html')
+    # - Select query to get details
+    name, age, s_d, location, role, email, gender, number, password = exampleUser[1:]
+    if role == "Disabled":
+        s_d = "Disability: " + s_d
+    else:
+        s_d = "Speciality: " + s_d
+
+    return render_template('two/profile.html', name=name, age=age, s_d=s_d, location=location, role=role, email=email,
+                           gender=gender, number=number, password=password)
 
 
 if __name__ == "__main__":
